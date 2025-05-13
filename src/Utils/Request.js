79d2f3ee144db1174f741ecc0522975d0b47f8be
minhnghia2k3 <88654111+minhnghia2k3@users.noreply.github.com>
2025-05-13 23:38:32 +0700

    update domain

--- src/Utils/Request.js
+++ src/Utils/Request.js
@@ -1,77 +1,69 @@
-const domain = process.env.BACKEND_URL;
+const domain = process.env.REACT_APP_BACKEND_URL;
 
 export const GetData = async (path) => {
-    try {
-        const response = await fetch(domain + path, {
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-        });
+  try {
+    const response = await fetch(domain + path, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
 
-
-
-        return await response.json();
-    } catch (error) {
-        return {
-            status: false,
-            type: "Auth",
-            error: 600,
-            data: null
-        }
-
-
-    }
+    return await response.json();
+  } catch (error) {
+    return {
+      status: false,
+      type: "Auth",
+      error: 600,
+      data: null,
+    };
+  }
 };
 
 export const PostData = async (path, option) => {
-    try {
-        const response = await fetch(domain + path, {
-            method: "POST",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-            body: JSON.stringify(option),
-        });
+  try {
+    const response = await fetch(domain + path, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(option),
+    });
 
-
-
-        return await response.json();
-    } catch (error) {
-        return {
-            status: false,
-            type: "Auth",
-            error: 600,
-            data: null
-        }
-
-    }
+    return await response.json();
+  } catch (error) {
+    return {
+      status: false,
+      type: "Auth",
+      error: 600,
+      data: null,
+    };
+  }
 };
 
 export const PatchData = async (path, option) => {
-    try {
-        const response = await fetch(domain + path, {
-            method: "PATCH",
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-            body: JSON.stringify(option),
-        });
+  try {
+    const response = await fetch(domain + path, {
+      method: "PATCH",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(option),
+    });
 
-
-        return await response.json();
-    } catch (error) {
-        return {
-            status: false,
-            type: "Auth",
-            error: 600,
-            data: null
-        }
-    }
+    return await response.json();
+  } catch (error) {
+    return {
+      status: false,
+      type: "Auth",
+      error: 600,
+      data: null,
+    };
+  }
 };
